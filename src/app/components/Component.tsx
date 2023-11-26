a7ee1b2048ec3e8d12169c549aa9acff62b0973b
ajmnz <arnau@ajb.cat>
2023-11-26 23:54:28 +0100

    initial commit

---
+++ src/app/components/Component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface ComponentProps {}
+
+export const Component: React.FC<ComponentProps> = () => {
+  return <></>;
+};
---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <h1>
+      We should be seeing this custom 404, but I you won't unless you change
+      /src/app to /src/somethingelse
+    </h1>
+  );
+}
